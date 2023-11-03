96021677e239ff4716af53bbeb0838a2d9d7cd1c
Yader Barahona <yaderbarahona180@gmail.com>
2023-11-02 22:44:31 -0600

    Actualizando el controlador Unidad con metodos geetall, getbyid, post, put y delete.

--- ApiGuanaHospi/Controllers/UnidadController.cs
+++ ApiGuanaHospi/Controllers/UnidadController.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Context;
+using DataAccess.DTO;
 using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,39 +15,108 @@ namespace ApiGuanaHospi.Controllers
             _context = context;
         }
 
+        //[HttpGet]
+        //public List<Unidad> GetAllUnidad()
+        //{
+        //    var unidades = _context.unidad.FromSqlInterpolated($"SP_ObtenerUnidad").ToList();
+        //    foreach (var unidad in unidades) { 
+        //        _context.Entry(unidad).Reference(u=>u.Doctor).Load();
+        //    }
+        //    return unidades;
+        //}
+
         [HttpGet]
-        public List<Unidad> Get()
+        public List<Unidad> GetAllUnidades()
         {
-            List<Unidad> unidades = _context.unidad.FromSql($"SP_ObtenerUnidad").ToList();
-            foreach (var unidad in unidades) { 
-                _context.Entry(unidad).Reference(u=>u.Doctor).Load();
+            var unidades = _context.unidad.FromSqlInterpolated($"SP_ObtenerUnidad").ToList();
+
+            foreach (var unidad in unidades)
+            {
+                _context.Entry(unidad)
+                    .Reference(u => u.Doctor)
+                    .Load();
+
             }
+
             return unidades;
         }
 
-        // GET api/<UnidadController>/5
+
+
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult GetUnidadById(int id)
         {
-            return "value";
+            var unidad = _context.unidad
+            .FromSqlInterpolated($"EXEC SP_ObtenerUnidadPorId {id}")
+            .AsEnumerable()
+            .SingleOrDefault();
+
+            if (unidad == null)
+            {
+                return NotFound();
+            }
+
+            if (unidad != null)
+            {
+                _context.Entry(unidad).Reference(u => u.Doctor).Load();
+            }
+
+            return Ok(unidad);
         }
 
-        // POST api/<UnidadController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult PostUnidad(UnidadDTO unidadDTO)
         {
+            var unidad = new Unidad
+            {
+                Codigo = unidadDTO.Codigo,
+                Nombre = unidadDTO.Nombre,
+                Planta = unidadDTO.Planta,
+                Id_Doctor = unidadDTO.ID_Doctor,
+                //null para no pasar nada al objeto de la relacion 
+                Doctor = null
+            };
+
+            _context.Database.ExecuteSqlInterpolated($"SP_InsertarUnidad {unidad.Codigo},{unidad.Nombre},{unidad.Planta},{unidad.Id_Doctor}");
+
+            return Ok("Unidad creada exitosamente");
         }
 
-        // PUT api/<UnidadController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult PutUnidad(int id, [FromBody] UnidadDTO unidadDTO)
         {
+            var existingUnidad = _context.unidad.FirstOrDefault(d => d.ID_Unidad == id);
+
+            if (existingUnidad == null)
+            {
+                return NotFound();
+            }
+
+            existingUnidad.Codigo = unidadDTO.Codigo;
+            existingUnidad.Nombre = unidadDTO.Nombre;
+            existingUnidad.Planta = unidadDTO.Planta;
+            existingUnidad.Id_Doctor = unidadDTO.ID_Doctor;
+
+            _context.Database.ExecuteSqlInterpolated($"SP_ActualizarUnidad {id}, {existingUnidad.Codigo},{existingUnidad.Nombre},{existingUnidad.Planta},{existingUnidad.Id_Doctor}");
+
+            _context.SaveChanges();
+
+            return NoContent();
         }
 
-        // DELETE api/<UnidadController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult DeleteUnidad(int id)
         {
+            var existingUnidad = _context.unidad.FirstOrDefault(d => d.ID_Unidad == id);
+
+            if (existingUnidad == null)
+            {
+                return NotFound();
+            }
+
+            _context.Database.ExecuteSqlInterpolated($"SP_EliminarUnidad {id}");
+
+            return NoContent();
         }
     }
 }
