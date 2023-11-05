a00c523e66fd9d8a39cf2f64e2eb0054237e8226
Kuc <kucsen2003@gmail.com>
2023-11-05 11:41:35 -0600

    AuthenticationController

---
+++ ApiGuanaHospi/Controllers/AuthenticationController.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace ApiGuanaHospi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : Controller
+    {
+        private readonly GuanaHospiContext _context;
+
+        public AuthenticationController(GuanaHospiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Login")]
+        public async Task<object> Login(string email, string password)
+        {
+            var usuario = new Usuario();
+            var rol = new Rol();
+            var usuarios = _context.usuario
+                .FromSqlRaw("sp_Login", email, password);
+            var resultado = ("sp_Login", email, password);
+
+            // Agregar la información del usuario a la respuesta
+            resultado.Result["idUsuario"] = Usuario.Id_Usuario;
+            resultado.Result["idRol"] = Usuario.ID_Rol;
+            resultado.Result["rolNombre"] = Rol.Nombre;
+
+            return resultado;
+        }
+    }
+}
