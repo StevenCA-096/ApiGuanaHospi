29019db90720b64b74b32fe013c9852db94e93b2
Steven Cordero A <scordero096@gmail.com>
2023-11-07 17:11:44 -0600

    Fixed getLoginInfo

--- DataAccess/DTO/UsuarioDto.cs
+++ DataAccess/DTO/UsuarioDto.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿using AutoMapper;
+using DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DataAccess.DTO
 {
     public class UsuarioDto
     {
-        public string Correro { get; set; }
+        public string Correo { get; set; }
         public string Contra { get; set; }
         public int ID_Rol { get; set; }
+    }
+    public class UsuarioMapper : Profile
+    {
+        public UsuarioMapper()
+        {
+            CreateMap<UsuarioDto, Usuario>()
+                .ForMember(dest => dest.Correo, opt => opt.MapFrom(src => src.Correo))
+                .ForMember(dest => dest.Contra, opt => opt.MapFrom(src => src.Contra))
+                .ForMember(dest => dest.ID_Rol, opt => opt.MapFrom(src => src.ID_Rol))
+                .ForMember(dest => dest.rol, opt => opt.Ignore());
+        }
     }
 }
---
+++ Services/IRepository/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.IRepository
+{
+    public interface IUserRepository : IGenericRepository<Usuario>
+    {
+        public Task<object> Login(string email, string password);
+        public Task<object> getLoginInfo(string email, string password);
+        public string getChangePasswordToken(string email);
+        //public Task<Usuario> getUserById(int id);
+        public string ChangePassword(string email, string newPassword);
+    }
+}
